e6790a5d9c18e0b2341308a05c77e20de9a272d7
sergiomarchena16 <mar16387@uvg.edu.gt>
2018-02-08 16:06:29 -0600

    main working

--- MainSorts.java
+++ MainSorts.java
@@ -4,8 +4,12 @@
  * and open the template in the editor.
  */
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.io.UnsupportedEncodingException;
 /**
  *
@@ -18,22 +22,45 @@ public class MainSorts{
      */
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
         
+        //GENERA UN ARCHIVO CON NUMEROS RANDOM.
         try {
+            PrintWriter writer = new PrintWriter("nums.txt", "UTF-8");
+            Random randomNum = new Random();
+            for (int i=1; i<=20; i++){
+                int randomInt = randomNum.nextInt(100);
+                writer.println(randomInt); 
+            }
+            writer.close();
             
-        PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
-        writer.println("The first line");
-        writer.println("The second line");
-        writer.close();
-        
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        int array[] = {34, 2, 10, -9, 334, 23, 0, -3, 23, 45, 100, 20, 40, 22, -12, 39, 45, 56, 346 -34, 34};
         
-        Sorts.gnomeSort(array, array.length);
+        //LEE EL ARCHIVO.TXT (nums.txt) Y LO GUARDA EN UN ARRAY.
+        FileReader file = new FileReader("nums.txt");
+        int[] integers = new int [20];
+        int i=0;
+        try {
+            Scanner input = new Scanner(file);
+            while(input.hasNext())
+            {
+                integers[i] = input.nextInt();
+                i++;
+            }
+            input.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        } 
         
+        System.out.println("Not sorted:" +Arrays.toString(integers));
+        Sorts.gnomeSort(integers, integers.length);
         System.out.print("Gnome sorted: ");
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(integers));
+        
     }
-}  
+    
+        
+       
+    }  
     
